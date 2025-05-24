8604f822137d493deea3a4dd777fc18130e0ddc2
0p3nt3ch53C <186238699+0p3nt3ch53C@users.noreply.github.com>
2025-05-24 00:59:55 -0400

    Update to use ollama

--- setup.sh
+++ setup.sh
@@ -8,4 +8,9 @@ curl -sSL https://get.docker.com | sh
 sudo apt-get install -y uidmap
 dockerd-rootless-setuptool.sh install
 
+# Install ollama: 
 curl -fsSL https://ollama.com/install.sh | sh
+# NOTE: WARNING: No NVIDIA/AMD GPU detected. Ollama will run in CPU-only mode.
+
+# Install Gemma 3 1B:
+ollama run gemma3:1b
